6fec729ac9ca41be9e7feac0cbc5ee24488daf4d
CarinaLR <carinalopez@Carinas-MacBook-Pro.local>
2020-06-30 14:41:20 -0400

    add general channel

--- static/script/index.js
+++ static/script/index.js
@@ -7,17 +7,19 @@ function hello() {
   }
 }
 
+//function to start the active chatroom.
 $(function () {
+  //Set variables to use in chat.
   let socket = io.connet(
     location.protocol + "//" + document.domain + ":" + location.port
   );
   privateWindow = false;
   inRoom = false;
-
+  //When chat is active
   socket.on("connect", () => {
     $("#input_msg").on("keyup", function (key) {
       active_channel = $("#channels_history .active").attr("id");
-
+      //Broadcast to all
       if (
         key.keyCode === 13 &&
         $(this).val() !== "" &&
@@ -35,7 +37,7 @@ $(function () {
           time: time,
         });
       }
-
+      //Send to channel
       if (
         key.keyCode === 13 &&
         $(this).val() !== "" &&
@@ -53,6 +55,7 @@ $(function () {
           user: user,
           time: time,
         });
+        //Send to private channel
       } else if (
         key.keyCode === 13 &&
         $(this).val !== "" &&
@@ -72,6 +75,74 @@ $(function () {
           time: time,
         });
       }
+    });
+    //Start list of channels
+    $("#channels_history").on("click", "li", function () {
+      $("#input_msg").focus();
+      if (!localStorage.getItem("active_channel")) {
+        active_channel = "General";
+      } else {
+        active_channel = localStorage.getItem("active_channel");
+      }
+      //Set variables
+      const user = localStorage.getItem("user");
+      const time = new Date().toLocaleString();
+
+      $(this).addClass("active");
+      $(this).siblings().removeClass("active");
+      $("#messages").html("");
+      //Announce that user has leave the room
+      if (active_channel !== "General" && !privateWindow) {
+        socket.emit("leave", {
+          channel: active_channel,
+          current_msg: "user disconnected",
+          user: user,
+          time: time,
+        });
+      }
+
+      active_channel = $("#channels_history .active").attr("id");
+      localStorage.setItem("active_channel", active_channel);
+      //Announce that come back to general channel
+      if (active_channel === "General") {
+        inRoom = false;
+        privateWindow = false;
+        return socket.emit("general channel");
+      } else {
+        inRoom = true;
+        privateWindow = false;
+      }
+      socket.emit("join", {
+        channel: active_channel,
+        current_msg: "initiate room",
+        user: user,
+        time: time,
+      });
+    });
+    //Prevent the user to close modal.
+    if (!localStorage.getItem("user")) {
+      $("#myModay").modal({ backdrop: "static", keyboard: false });
+      $(".modal-title").text("Please enter your username");
+      $("#modalInput").val("");
+    }
+  });
+
+  socket.on("announce to all", (data) => {
+    if (!privateWindow) {
+      loadMessages(data);
+    }
+
+    $(".text-danger").on("click", () => {
+      chooseUser($(this).text());
+    });
+  });
+
+  socket.on("joined", (data) => {
+    loadMessages(data);
+
+    $("#input_msg").foucs();
+    $(".text-danger").on("click", () => {
+      chooseUser($(this).text());
     });
   });
 });
